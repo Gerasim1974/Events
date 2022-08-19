172aeb055c80ebeefe21e0bc3e384869402cef20
Gerasim1974 <109235581+Gerasim1974@users.noreply.github.com>
2022-08-19 18:55:33 +0300

    Events

---
+++ HomeWorkEvents/slnHomeVorkEvents/appHomeVorkEvents/Microwave.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appHomeVorkEvents
+{
+    public delegate void NotifyWarmUpComplited(string nameFood);
+    internal class Microwave
+    {
+        public event NotifyWarmUpComplited OnWarmUpComplited;  
+
+        public void WarmUp(string nameFood)
+        { 
+            Console.WriteLine($"Подогреваю {nameFood}");
+            if (OnWarmUpComplited != null) {OnWarmUpComplited(nameFood); }
+        }
+    }
+}
---
+++ HomeWorkEvents/slnHomeVorkEvents/appHomeVorkEvents/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace appHomeVorkEvents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Microwave microwave = new Microwave();
+            microwave.OnWarmUpComplited += WarmUpCompletedHandler;
+
+            microwave.WarmUp("Хот-Дог");
+        }
+        static void WarmUpCompletedHandler(string nameFood)
+        {
+            Console.WriteLine($"Ваша еда готова: {nameFood}");
+        }
+    }
+}
